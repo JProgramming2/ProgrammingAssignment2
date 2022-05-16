31e2b52e521aaa242edde7dc77621580fadd16e2
JProgramming2 <105612798+JProgramming2@users.noreply.github.com>
2022-05-16 17:25:12 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+#John Hopkins R Programming Assignment 2 Week 3
+#I'd appreciate any feedback as I am still somewhat confused
 
-## Write a short comment describing this function
+#This function is for creating matrices (regular and inv) and cache them
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+    inverse <<- NULL
+    set <- function(y){ 
+      x <<- y
+      inverse <- NULL
+    }
+    get <- function()x
+    invSet <- function(inv) inverse <<- inv
+    invGet <- function()inv
+    list(set = set, get = get,
+       invSet = invSet,
+       invGet = invGet) 
 }
 
-
-## Write a short comment describing this function
+#The function is for caching the inv of the makeCacheMatrix and 
+#return matrix of inv
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  z <- x[[invGet()]]
+  if(!is.null(z)) {
+    message("receiving cached data")
+    return(z)
+  }
+  data <- x[[get()]]
+  z <- inverse(data, ...)
+  x[[invSet(z)]]
+  z
 }
+
+#test code
+
+c <- makeCacheMatrix(matrix(1:10,3,9))
+cacheSolve(c)
+
